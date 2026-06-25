fac41ab88abbd0c5973e284f18477e0589930284
Anurag Soni <anurag1201soni@gmail.com>
2026-06-25 22:41:50 +0530

    Master in Opps

--- Java Script/part2/Object_Oriented_P/oop-master.js
+++ Java Script/part2/Object_Oriented_P/oop-master.js
@@ -1,3 +1,4 @@
+//01
 let car = {
   make: "Toyota",
   model: "Camry",
@@ -6,16 +7,22 @@ let car = {
     return `${this.make} car got started in ${this.year}`;
   },
 };
-// console.log(car.start());
+ console.log(car.start());    // ! Note : yaha () small bracet es liye kyuki dhyan se dekho star funtion hai agr  nahi hota tho bracket nahi lagate 
 
+//02
 function Person(name, age) {
   this.name = name;
   this.age = age;
 }
 
 let john = new Person("John Doe", 20);
-// console.log(john.name);
+ console.log(john.name);   // jese yaha koi function nahi hai alag se john mein tho bracket nahi lagega 
+ console.log(john.age);     
+ console.log(john);
 
+
+
+//03
 function Animal(type) {
   this.type = type;
 }
@@ -24,15 +31,36 @@ Animal.prototype.speak = function () {
   return `${this.type} makes a sound`;
 };
 
-Array.prototype.hitesh = function () {
+Array.prototype.aaku = function () {
   return `Custom method ${this}`;
 };
 
-let myArray = [1, 2, 3];
-// console.log(myArray.hitesh());
-let myNewArray = [1, 2, 3, 4, 5, 6];
-// console.log(myNewArray.hitesh());
+let janwar = new Animal("Labradour")
+ console.log(janwar);
+ console.log(janwar.speak());
 
+let myArray = [1, 2, 3];
+ console.log(myArray.aaku());
+let myNewArray = [1, 2, 3, 4, 5, 6];
+ console.log(myNewArray.aaku());
+
+
+
+
+ /*            OOP
+
+      ┌──────────────┐
+      │ Encapsulation│
+      ├──────────────┤
+      │ Abstraction  │
+      ├──────────────┤
+      │ Inheritance  │
+      ├──────────────┤
+      │ Polymorphism │
+      └──────────────┘
+*/
+
+//04   Inheritance 
 class Vehicle {
   constructor(make, model) {
     this.make = make;
@@ -44,39 +72,173 @@ class Vehicle {
   }
 }
 
-class Car extends Vehicle {
+class Car extends Vehicle {    //! here we are doing a inhetance , geeting some  values from vechicle class   so by using  [exdent] we can inherit 
   drive() {
     return `${this.make} : This is an inheritance example`;
   }
 }
 
 let myCar = new Car("Toyota", "Corolla");
-// console.log(myCar.start());
-// console.log(myCar.drive());
+ console.log(myCar.start());
+ console.log(myCar.drive());
 
 let vehOne = new Vehicle("Toyota", "Corolla");
-// console.log(vehOne.make);
+ console.log(vehOne.make);
 
-// Encapsulation
 
-class BankAccount {
-  #balance = 0;
+/*1. Encapsulation
 
-  deposit(amount) {
-    this.#balance += amount;
-    return this.#balance;
-  }
+    Encapsulation means:   Data aur methods ko ek hi box (class/object) ke andar rakhna.
+    
+    Real Life Example
+    Phone 📱
+    Tum phone use karte ho,  Tum dekhte ho
+    
+    Call Button
+    Camera
+    Gallery
+    Settings
+    
+    Lekin andar kya chal raha hai?
+    
+    Processor
+    RAM
+    CPU
+    Algorithms
+    
+    Tumhe nahi pata.
 
-  getBalance() {
-    return `$ ${this.#balance}`;
-  }
-}
+    Sab ek box ke andar pack hai.  -->  Ye hi encapsulation hai.
+    
+    Without Encapsulation*/
+    let name = "John";
+    let age = 20;
+    
+    function greet(){
+    }
+    
+ //Sab alag-alag pada hua hai.  With Encapsulation
+    class Person{
+    
+        constructor(name,age){
+            this.name = name;
+            this.age = age;
+        }
+    
+        greet(){
+            console.log("Hello");
+        }
+    
+    }
+ /*Diagram
 
-let account = new BankAccount();
+                Person
+      ┌──────────────────────────┐
+      │ name                     │
+      │ age                      │
+      │                          │
+      │ greet()                  │
+      └──────────────────────────┘
+      
+      Memory      
+      person1
+      ┌─────────────────────┐
+      │ name = John         │
+      │ age = 20            │
+      │ greet()             │
+      └─────────────────────┘
+ */
+        class BankAccount {
+      #balance = 0;
+    
+      deposit(amount) {
+        this.#balance += amount;
+        return this.#balance;
+      }
+    
+      getBalance() {
+        return `$ ${this.#balance}`;
+      }
+    }
+    
+    let account = new BankAccount();
 // console.log(account.getBalance());
 
-// Abstraction
 
+
+
+
+/*2. Abstraction
+
+Abstraction means
+      
+      Show only what is necessary.
+      Hide unnecessary implementation.
+      
+      Real Life
+      
+      Car
+      Tum sirf:
+      
+      Start Button
+      Brake
+      Steering
+
+      use karte ho.
+      
+      Tumhe ye nahi pata
+      Fuel Injection
+      Engine Timing
+      Gear Mechanism
+      
+      JavaScript me bhi same.
+      
+      Example*/
+      
+      class Car{
+      
+          start(){
+              console.log("Car Started");
+          }
+      
+      }
+      
+    /*  User sirf     car.start();   karta hai.    Usko andar kya ho raha hai pata nahi.
+      
+  Diagram
+      
+      User   
+      car.start()
+            │
+            ▼
+      ┌──────────────┐
+      │ Car          │
+      │              │
+      │ Engine code  │
+      │ Fuel system  │
+      │ Sensors      │
+      └──────────────┘    
+      Hidden.
+      
+      Ye abstraction hai.
+      
+      Flow
+      User
+       │
+       ▼
+      start()
+       │
+       ▼
+      Internal Logic
+       │
+       ▼
+      Car Starts
+
+                  Difference
+      Encapsulation  -->   Pack everything together.
+      
+      Abstraction    -->   Hide unnecessary details.
+  */
 class CoffeMachine {
   start() {
     // call DB
